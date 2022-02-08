55692a25929e1c8935a0bca23951d8c9a150e266
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-08 12:59:35 +0200

    initial commit

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def get(Routes, request):
+    return JsonResponse({
+        'status': 'success',
+        'message': 'Hello, World!'
+    })
