b84ad97cbb067731f003b450e6cfdddb79251fa9
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-08 15:40:16 +0200

    adding serializers

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .serializers import NoteSerializer
+from .models import Note
 
 @api_view(['GET'])
 def getRoutes(request):
