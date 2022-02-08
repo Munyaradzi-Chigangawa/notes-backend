127c810f1084d10944cb30ab8de86671925b9577
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-08 13:38:47 +0200

    api

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.Apps.ApiConfig',
+
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
