5ed1fe8a98195fe32cba4a5f7b7514e8cfcef5db
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-08 16:15:11 +0200

    added POST request

--- notes/api/urls.py
+++ notes/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes),
     path('notes/', views.getNotes),
+    path('notes/create/', views.createNote),
+    path('notes/<str:pk>/', views.getNote),
 ]
