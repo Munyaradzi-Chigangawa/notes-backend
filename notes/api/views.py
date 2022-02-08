4f71d52202d74cf8abca9cea3f541b21f962e407
Munyaradzi Chigangawa <munyaradzichigangawa6@gmail.com>
2022-02-08 13:24:41 +0200

    api

--- notes/api/views.py
+++ notes/api/views.py
@@ -1,7 +1,24 @@
 from django.http import JsonResponse
 
-def get(Routes, request):
-    return JsonResponse({
-        'status': 'success',
-        'message': 'Hello, World!'
-    })
+def getRoutes(request):
+    routes = [
+        {
+            'Endpoint' : '/notes/',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Return Array of notes'
+        },
+        {
+            'Endpoint' : '/notes/id',
+            'method' : 'GET',
+            'body' : None,
+            'description' : 'Returns single note with specified id',
+        },
+        {
+            'Endpoint' : '/notes/create',
+            'method' : 'POST',
+            'body' : {'body': ""},
+            'description' : 'Creates a new post with specified info'
+        },
+    ]
+    return JsonResponse(routes)
